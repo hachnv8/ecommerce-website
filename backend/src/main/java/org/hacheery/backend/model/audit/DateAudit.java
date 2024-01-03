9d76e4e88b696e3277fd760b0791de1d6ff40cde
hachnv8 <hachnv8@gmail.com>
2024-01-03 23:05:10 +0700

    change frontend

--- backend/src/main/java/org/hacheery/backend/model/audit/DateAudit.java
+++ backend/src/main/java/org/hacheery/backend/model/audit/DateAudit.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -25,11 +26,13 @@ public abstract class DateAudit implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@CreatedDate
+	@CreationTimestamp
 	@Column(nullable = false, updatable = false)
 	private Instant createdAt;
 
 	@LastModifiedDate
 	@Column(nullable = false)
+	@CreationTimestamp
 	private Instant updatedAt;
 
 }
--- backend/src/main/java/org/hacheery/backend/security/service/AuthenticationService.java
+++ backend/src/main/java/org/hacheery/backend/security/service/AuthenticationService.java
@@ -34,7 +34,7 @@ public class AuthenticationService {
                 .role(Role.USER)
                 .build();
         var savedUser = repository.save(user);
-        var jwtToken = jwtService.generateToken(user);
+        var jwtToken = jwtService.generateToken(savedUser);
         saveUserToken(savedUser, jwtToken);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
